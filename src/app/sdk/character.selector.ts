14dc5fb764b5d4d648857c26307c64be18ebd2d5
Levente Szabo <levente.szabo@betssongroup.com>
2023-03-29 11:13:45 +0200

    Updated to ng14

--- src/app/sdk/character.selector.ts
+++ src/app/sdk/character.selector.ts
@@ -1,9 +1,1 @@
 // You need to add the necessary selector that retrieves the data from the store and the component can use.
-import { Injectable } from "@angular/core";
-
-@Injectable({
-	providedIn: "root"
-})
-export class CharacterSelector {
-
-}
